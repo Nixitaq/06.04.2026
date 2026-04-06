6b74b5432515e399ad1f0cc2458ff82fbd1e9259
Nikita <prayde.polyakov@gmail.com>
2026-04-06 11:53:07 +0300

    homeW

--- src/components/Task1.js
+++ src/components/Task1.js
@@ -1,16 +1,31 @@
 
-export  function Home(){
-    return <h2>Главная</h2>;
+export function Home() {
+  return (
+    <div className="container">
+      <div className="Image">
+        <img src="faceit.png" alt="фото" />
+      </div>
+      <div className="Text">
+        <p>Магазин игровых девайсов "CyberZone"</p>
+        <p>Работаем ежедневно с 10:00 до 22:00</p>
+        <p>Лучшие цены на периферию в вашем городе</p>
+      </div>
+    </div>
+  );
 }
+
 export  function About(){
     return <h2>About</h2>;
 }
+
 export  function Products(){
     return <h2>Products</h2>;
 }
+
 export  function Contacts(){
     return <h2>Contacts</h2>;
 }
+
 export  function Info(){
     return <h2>Info</h2>;
 }
--- src/components/Task2.js
+++ src/components/Task2.js
@@ -1,35 +1,34 @@
-import { Link, Outlet  } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export const MainPage = () => (
-    <>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/home">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/Products">Products</Link>
-      </li>
-      <li>
-        <Link to="/Contacts">Contacts</Link>
-      </li>
-      <li>
-        <Link to="/Info">Info</Link>
-      </li>
-    </ul>  
-  </nav>
-   <hr/> 
-{/* !!!!!!!!!!!!!!!!!!!!!! */}
+  <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/Products">Products</Link>
+        </li>
+        <li>
+          <Link to="/Contacts">Contacts</Link>
+        </li>
+        <li>
+          <Link to="/Info">Info</Link>
+        </li>
+      </ul>
+    </nav>
+    <hr />
+    {/* !!!!!!!!!!!!!!!!!!!!!! */}
+    <div id='main'>
+      <Outlet />
+    </div>
+    {/* !!!!!!!!!!!!!!!!!!!!!! */}
 
-  <Outlet/> 
-  
-{/* !!!!!!!!!!!!!!!!!!!!!! */}
-  <p>Hello world !!!!</p>
-  
-  <hr/>  
+    <hr />
   </>
 
 );
